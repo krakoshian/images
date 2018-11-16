95f6d826d7dd525f378933ec8817db4e0319729c
Kadir Uludağ <kadiruludag@kadir-macbook-pro.local>
2018-11-16 15:20:34 +0300

    Initial Commit

---
+++ images/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  images
+//
+//  Created by Kadir Uludağ on 16.11.2018.
+//  Copyright © 2018 Kadir Uludağ. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
